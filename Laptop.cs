fa8ce6c51d5fcc02dfe49d56bdb570a14d945a04
Udvarhelyi Zsombor <53795132+ZsomborUdvarhelyi@users.noreply.github.com>
2025-02-11 14:00:19 +0100

    Add project files.

---
+++ Laptop.cs
@@ -0,0 +1,31 @@
+﻿namespace LaptopsCLI
+{
+    internal class Laptop
+    {
+        public Category Category { get; set; }
+        public string CPU { get; set; }
+        public Manufacturer Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string OS { get; set; }
+        public string Price { get; set; }
+        public int RAM { get; set; }
+        public string Screen { get; set; }
+        public string Storage { get; set; }
+
+        public override string ToString() => $"{Manufacturer.ManufacturerName} {Model}";
+
+        public Laptop(string s)
+        {
+            var v = s.Split(';');
+            Category = new Category( int.Parse(v[0]), v[1] );
+            CPU = v[2];
+            Manufacturer = new Manufacturer ( int.Parse(v[3]), v[4] );
+            Model = v[5];
+            OS = v[6];
+            Price = v[7];
+            RAM = int.Parse(v[8]);
+            Screen = v[9];
+            Storage = v[10];
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using LaptopsCLI;
+using System.IO;
+using System.Windows;
+
+namespace LaptopsGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Laptop> laptops = [];
+        private int viewedCount = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+            using StreamReader sr = new(@"..\..\..\src\laptops.txt");
+            _ = sr.ReadLine();
+            while (!sr.EndOfStream) laptops.Add(new Laptop(sr.ReadLine()!));
+
+            var manufacturers = laptops.GroupBy(l => l.Manufacturer.ManufacturerName).Count();
+            var totalLaptops = laptops.Count;
+            tbSummary.Text = $"{manufacturers} gyártó {totalLaptops} gépe közül választhat.";
+
+
+            lbLaptops.ItemsSource = laptops;
+        }
+
+        private void LbLaptops_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (lbLaptops.SelectedIndex >= 0)
+            {
+                var selectedLaptop = laptops[lbLaptops.SelectedIndex];
+                tbDetails.Text = GetLaptopDetails(selectedLaptop);
+                viewedCount++;
+
+            }
+        }
+
+        private string GetLaptopDetails(Laptop laptop)
+        {
+            var screenParts = laptop.Screen.Split(new[] { ' ', '-' }, StringSplitOptions.RemoveEmptyEntries);
+            var screenSize = screenParts[0];
+            var resolution = screenParts.Last();
+
+            var priceInHUF = Math.Round(double.Parse(laptop.Price.Replace(",", ".")) * 4.12);
+
+            return $"Kategória\n\t{laptop.Category.CategoryName}\n" +
+                   $"Képátló\n\t{screenSize}\"\n" +
+                   $"Felbontás\n\t{resolution}\n" +
+                   $"Processzor\n\t{laptop.CPU}\n" +
+                   $"RAM\n\t{laptop.RAM} GB\n" +
+                   $"Háttértár(ak)\n\t{laptop.Storage}" +
+                   $"\nOperációs rendszer\n\t{laptop.OS}" +
+                   $"\nÁr\n\t{priceInHUF} Ft";
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show($"Ön {viewedCount} terméket tekintett meg. Bezárja az alkalmazást?", "Kilépés", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
